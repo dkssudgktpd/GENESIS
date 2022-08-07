d21c805b4521c56cdecf402ea4c33de93d5334ad
moon <anstnqls554@gmail.com>
2022-08-07 22:47:10 +0900

    헤더 미완성

--- js/script.js
+++ js/script.js
@@ -1,3 +1,58 @@
-window.onload = function(){
-  
+window.onload = function () {
+    let mainMenu = $('.main-menu');
+    let headerDepth2 = $('.header-depth2');
+    $.each(mainMenu,function(index, item){
+        $(this).click(function(event){
+            event.preventDefault();
+            headerDepth2.slideUp(1);
+            headerDepth2.eq(index).stop().slideToggle();
+
+        })
+    })
+
+
+    new Swiper('.sw-model1', {
+        pagination: {
+            el: '.sw-model-pg'
+        },
+        navigation: {
+            prevEl: '.sw-header-prev',
+            nextEl: '.sw-header-next'
+        },
+        allowTouchMove: false,
+        observer: true,
+        observeParents: true,
+    });
+    // 헤더 모델의 탭메뉴
+    let model_List_All = $('.model-list-all');
+    let model_List_Sedan = $('.model-list-sedan');
+    let model_List_Suv = $('.model-list-suv');
+    let model_All = $('.model-all');
+    let model_Sedan = $('.model-sedan');
+    let model_Suv = $('.model-suv');
+
+    model_List_All.click(function () {
+        model_Sedan.hide();
+        model_Suv.hide();
+        model_All.show();
+        model_List_Sedan.removeClass('model-list-active');
+        model_List_Suv.removeClass('model-list-active');
+        $(this).addClass('model-list-active');
+    });
+    model_List_Sedan.click(function () {
+        model_Sedan.show();
+        model_Suv.hide();
+        model_All.hide();
+        model_List_All.removeClass('model-list-active');
+        model_List_Suv.removeClass('model-list-active');
+        $(this).addClass('model-list-active');
+    });
+    model_List_Suv.click(function () {
+        model_Sedan.hide();
+        model_Suv.show();
+        model_All.hide();
+        model_List_Sedan.removeClass('model-list-active');
+        model_List_All.removeClass('model-list-active');
+        $(this).addClass('model-list-active');
+    });
 }
